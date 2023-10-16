c32a9c19d03d8e4372102dc2b19108c6b17a6517
Josh Newsome <50967636+jnewsome97@users.noreply.github.com>
2023-10-16 17:49:03 +0100

    Update setup-windows-demo.sh

--- support/setup-windows-demo.sh
+++ support/setup-windows-demo.sh
@@ -4,28 +4,28 @@
 windows_machineset=$(oc get machineset -n openshift-machine-api -o name | grep windows | cut -d/ -f2) &
 
 # Scale down the windows machine set to 0 using oc scale command
-oc scale machineset $windows_machineset --replicas=0 -n openshift-machine-api &>/dev/null &
+oc scale machineset $windows_machineset --replicas=0 -n openshift-machine-api &>/dev/null &&
 
 # Wait for the command to finish and print a message
 wait $! && echo "Step 1: Windows Machineset scaled" || echo "Step 1: Scaling Windows Machineset failed"
 
 # Download and install Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 &>/dev/null &
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 &>/dev/null &&
 
 # Make script executable
-chmod 700 get_helm.sh &>/dev/null &
+chmod 700 get_helm.sh &>/dev/null &&
 
 ./get_helm.sh &>/dev/null &
 # Wait for the command to finish and print a message or an error 
 wait $! && echo "Step 2: Installing Helm complete" || echo "Step 2: Installing Helm failed"
 
 # Delete the secret file named cloud-private-key in the namespace openshift-windows-machine-config-operator
-oc delete secret cloud-private-key -n openshift-windows-machine-config-operator &>/dev/null &
+oc delete secret cloud-private-key -n openshift-windows-machine-config-operator &>/dev/null &&
 # Wait for the command to finish and print a message or an error 
 wait $! && echo "Step 3: Deleting secret file complete" || echo "Step 3: Deleting secret file failed"
 
 # Make all files in the directory executable
-chmod +x /home/lab-user/windows-containers-quickstart/support/* &>/dev/null &
+chmod +x /home/lab-user/windows-containers-quickstart/support/* &>/dev/null &&
 # Wait for the command to finish and print a message or an error 
 wait $! && echo "Step 4: Make scripts executable" || echo "Step 4: Make scripts executable failed"
 
